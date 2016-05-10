ca42ed61c7081f6278ec70235c5ed11e5b1b5c4c
Alex Scott <0AlexScott@gmail.com>
2016-05-10 15:51:05 +0100

    Added a bunch of trophies and records, hard coded and added to view

--- www/js/controllers/awardsController.js
+++ www/js/controllers/awardsController.js
@@ -1,16 +1,23 @@
 ﻿angular.module('app.controllers').controller('AwardsCtrl',
-    ['$scope', 'listStorageService',
-     function ($scope, listStorageService) {
+    ['$scope', 'listStorageService', 'productivityService',
+     function ($scope, listStorageService, productivityService) {
 
          $scope.model = {};
-         $scope.model.hello = "ello ello ello";
+         $scope.model.trophies = [];
+         $scope.model.records = [];
 
          //add a 'loader' to page as it is opening
          $scope.$on('$ionicView.beforeEnter', function (e) {
+             $scope.model.trophies = [];
+             $scope.model.records = [];
          });
 
          //init tasks and add them to their respective lists when the view is entered
          $scope.$on('$ionicView.enter', function (e) {
+             productivityService.getAllAwards().then(function (awards) {
+                 $scope.model.trophies = awards.slice(0, 8);
+                 $scope.model.records = awards.slice(8, 12);
+             }, function (error) { });
          });
 
      }]);
--- www/js/services/productivityService.js
+++ www/js/services/productivityService.js
@@ -1,15 +1,103 @@
 ﻿angular.module('app.services').factory('productivityService', ['$http', '$q', '$timeout', '$rootScope', function ($http, $q, $timeout, $rootScope) {
 
     var service = {};
+    service.dayInMillis = 86400000;
 
     //Init productivity serivce, create table if none exist
     service.init = function () {
         if (!service.db) {
             service.db = window.sqlitePlugin.openDatabase({ name: "my.productivityPoints.db" });
             service.db.executeSql('CREATE TABLE IF NOT EXISTS productivity_transactions (id integer primary key, pointsAwarded integer, completionDate integer);');
-            service.db.executeSql('CREATE TABLE IF NOT EXISTS awards_table (id integer primary key, awardDescription text, );');
-
+            service.db.executeSql('CREATE TABLE IF NOT EXISTS awards_table (id integer primary key, awardName text, awardDescription text, count integer, lastAwarded integer, timeDelay integer);');
+            service.getAllAwards().then(function (rows) {
+                if (rows.length < 12) {
+                    service.populateAwards();
+                }
+            }, function (error) { });
         }
+    };
+
+    //first time the DB is opened, awards must be instantiated
+    service.populateAwards = function () {
+        console.log("FirstTimeOpen = true, awards being initialised");
+        service.createAward('Partially Productive','Obtained PP THREE days in a row', service.dayInMillis * 3);     //1
+        service.createAward('Powerfully Productive','Obtained PP SEVEN days in a row', service.dayInMillis * 7);    //2
+        service.createAward('Productivity Slugger','Obtained PP FOURTEEN days in a row', service.dayInMillis * 14); //3
+        service.createAward('Grafter','Worked for 200 PP in ONE day', service.dayInMillis);                         //4
+        service.createAward('Hard worker','Worked for 500 PP in ONE day!', service.dayInMillis);                    //5
+        service.createAward('What a day!','Completed 10 tasks in one day', service.dayInMillis);                    //6
+        service.createAward('Completionist','Completed 30 tasks in a week', service.dayInMillis * 7);               //7
+        service.createAward('Smashing records','Earned more PP than week before', service.dayInMillis * 7);         //8
+        service.createAward('Record','Most tasks completed in one day', service.dayInMillis);                       //9
+        service.createAward('Record','Most tasks completed in one week', service.dayInMillis * 7);                  //10
+        service.createAward('Record','PP gained in one day', service.dayInMillis);                                  //11
+        service.createAward('Record','PP gained in one week', service.dayInMillis * 7);                             //12
+    };
+
+    //check for completed Awards
+    service.checkForAwardCompletion = function () {
+        service.getAllAwards().then(function (awards) {
+            service.getAllTransactions().then(function(transactions){
+                var d = new Date();
+                var newAwards = [];
+
+
+
+                resolve();
+            }, function (error) {
+                reject(false);
+            })
+        }, function (error) {
+            reject(false);
+        })
+    };
+
+    //create award
+    service.createAward = function (awardName, awardDescription, timeDelay) {
+        var d = new Date();
+        var t = d.getTime();
+        return $q(function (resolve, reject) {
+            service.db.executeSql("INSERT INTO awards_table (awardName, awardDescription, count, lastAwarded, timeDelay) " +
+                "VALUES (?, ?, ?, ?, ?);", [awardName, awardDescription, 0, t, timeDelay], function (res) {
+                    console.log("Award added to DB: " + awardName);
+                    resolve(true);
+                }, function (error) {
+                    console.log('INSERT error: ' + error.message);
+                    reject(false);
+                });
+        });
+    };
+
+    //gets all awards from table
+    service.getAllAwards = function () {
+        return $q(function (resolve, reject) {
+            service.db.executeSql("SELECT * FROM awards_table;", [], function (res) {
+                var rows = res.rows;
+                var sanitizedRows = [];
+                for (var i = 0; i < rows.length; i++) {
+                    sanitizedRows[i] = rows.item(i);
+                    sanitizedRows[i].lastAwarded = new Date(parseInt(rows.item(i).lastAwarded, 10));
+                }
+                console.log("All awards selected");
+                resolve(sanitizedRows);
+            }, function (error) {
+                console.log('INSERT error: ' + error.message);
+                reject(false);
+            });
+        });
+    };
+
+    //function to update an award
+    service.updateAward = function (id, count, lastAwarded) {
+        return $q(function (resolve, reject) {
+            service.db.executeSql("UPDATE awards_table SET [count] = ?, [lastAwarded] = ?" +
+                "WHERE id=?;", [count, lastAwarded, id], function (res) {
+                    resolve(true);
+                }, function (error) {
+                    console.log('INSERT error: ' + error.message);
+                    reject(false);
+                });
+        });
     };
 
     //function to insert points into the database
@@ -28,6 +116,25 @@
         });
     };
 
+    //return all PP transactions
+    service.getAllTransactions = function () {
+        return $q(function (resolve, reject) {
+            service.db.executeSql("SELECT * FROM productivity_transactions;", [], function (res) {
+                var rows = res.rows;
+                var sanitizedRows = [];
+                for (var i = 0; i < rows.length; i++) {
+                    sanitizedRows[i] = rows.item(i);
+                    sanitizedRows[i].completionDate = new Date(parseInt(rows.item(i).completionDate, 10));
+                }
+                resolve(sanitizedRows);
+            }, function (error) {
+                console.log('INSERT error: ' + error.message);
+                reject(false);
+            });
+        });
+    };
+
+    //return total PP
     service.getTotalPoints = function () {
         return $q(function (resolve, reject) {
             service.db.executeSql("SELECT * FROM productivity_transactions;", [], function (res) {
@@ -43,7 +150,7 @@
                 reject(false);
             });
         });
-    };    
+    };
 
 
 
