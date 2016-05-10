1f8bad37fd0ae1adf1053790aa2f26ed3c1d7365
Alex Scott <0AlexScott@gmail.com>
2016-05-10 16:21:27 +0100

    Adding satisfaction clauses for awards (2/12)

--- www/js/services/productivityService.js
+++ www/js/services/productivityService.js
@@ -1,4 +1,4 @@
-﻿angular.module('app.services').factory('productivityService', ['$http', '$q', '$timeout', '$rootScope', function ($http, $q, $timeout, $rootScope) {
+﻿angular.module('app.services').factory('productivityService', ['$http', '$q', '$timeout', '$rootScope', '$state', '$ionicPopup', function ($http, $q, $timeout, $rootScope, $state, $ionicPopup) {
 
     var service = {};
     service.dayInMillis = 86400000;
@@ -20,41 +20,106 @@
     //first time the DB is opened, awards must be instantiated
     service.populateAwards = function () {
         console.log("FirstTimeOpen = true, awards being initialised");
-        service.createAward('Partially Productive','Obtained PP THREE days in a row', service.dayInMillis * 3);     //1
-        service.createAward('Powerfully Productive','Obtained PP SEVEN days in a row', service.dayInMillis * 7);    //2
-        service.createAward('Productivity Slugger','Obtained PP FOURTEEN days in a row', service.dayInMillis * 14); //3
-        service.createAward('Grafter','Worked for 200 PP in ONE day', service.dayInMillis);                         //4
-        service.createAward('Hard worker','Worked for 500 PP in ONE day!', service.dayInMillis);                    //5
-        service.createAward('What a day!','Completed 10 tasks in one day', service.dayInMillis);                    //6
-        service.createAward('Completionist','Completed 30 tasks in a week', service.dayInMillis * 7);               //7
-        service.createAward('Smashing records','Earned more PP than week before', service.dayInMillis * 7);         //8
-        service.createAward('Record','Most tasks completed in one day', service.dayInMillis);                       //9
-        service.createAward('Record','Most tasks completed in one week', service.dayInMillis * 7);                  //10
-        service.createAward('Record','PP gained in one day', service.dayInMillis);                                  //11
-        service.createAward('Record','PP gained in one week', service.dayInMillis * 7);                             //12
+        service.createAward('Partially Productive', 'Obtained PP THREE days in a row', service.dayInMillis * 3);     //1
+        service.createAward('Powerfully Productive', 'Obtained PP SEVEN days in a row', service.dayInMillis * 7);    //2
+        service.createAward('Productivity Slugger', 'Obtained PP FOURTEEN days in a row', service.dayInMillis * 14); //3
+        service.createAward('Grafter', 'Worked for 200 PP in ONE day', service.dayInMillis);                         //4
+        service.createAward('Hard worker', 'Worked for 500 PP in ONE day!', service.dayInMillis);                    //5
+        service.createAward('What a day!', 'Completed 10 tasks in one day', service.dayInMillis);                    //6
+        service.createAward('Completionist', 'Completed 30 tasks in a week', service.dayInMillis * 7);               //7
+        service.createAward('Smashing records', 'Earned more PP than week before', service.dayInMillis * 7);         //8
+        service.createAward('Record', 'Most tasks completed in one day', service.dayInMillis);                       //9
+        service.createAward('Record', 'Most tasks completed in one week', service.dayInMillis * 7);                  //10
+        service.createAward('Record', 'PP gained in one day', service.dayInMillis);                                  //11
+        service.createAward('Record', 'PP gained in one week', service.dayInMillis * 7);                             //12
     };
 
     //check for completed Awards
     service.checkForAwardCompletion = function () {
-        service.getAllAwards().then(function (awards) {
-            service.getAllTransactions().then(function(transactions){
-                var d = new Date();
-                var newAwards = [];
+        return $q(function (resolve, reject) {
+            service.getAllAwards().then(function (awards) {
+                service.getAllTransactions().then(function (transactions) {
+                    var newAwards = [];
 
+                    var d = new Date();
+                    var yesterday = new Date(d.getFullYear(), d.getMonth(), d.getDate() - 1, d.getMinutes(), d.getSeconds(), 0);
 
+                    if (service.canBeAwarded(awards[0])) {
+                        var toaward = true;
 
-                resolve();
+                        var twodaysago = new Date(d.getFullYear(), d.getMonth(), d.getDate() - 2, d.getMinutes(), d.getSeconds(), 0);
+                        var threedaysago = new Date(d.getFullYear(), d.getMonth(), d.getDate() - 3, d.getMinutes(), d.getSeconds(), 0);
+                        var timeFrames = [d, yesterday, twodaysago, threedaysago];
+                        for (var i = 0; i < timeFrames.length - 1; i++) {
+                            var complete = false;
+                            for (var j = 0; j < transactions.length; j++) {
+                                if (transactions[j].completionDate > timeFrames[i] && transactions[j] < timeFrames[i + 1]) {
+                                    complete = true;
+                                }
+                            }
+                            if (complete == false) {
+                                toaward = false;
+                            }
+                        }
+                        if (toaward) {
+                            service.updateAward(awards[0].id, awards[0].count + 1, d.getTime());
+                            newAwards.push(awards[0]);
+                        }
+                    }
+
+                    if (service.canBeAwarded(awards[3])) {
+                        var totalPPSinceYesterday = 0;
+
+                        for (var i = 0; i < transactions.length; i++) {
+                            if (transactions[j].completionDate > yesterday && transactions[j] < d) {
+                                totalPPSinceYesterday += transactions[j].pointsAwarded;
+                            }
+                        }
+
+                        if (totalPPSinceYesterday >= 200) {
+                            service.updateAward(awards[3].id, awards[3].count + 1, d.getTime());
+                            newAwards.push(awards[3]);
+                        }
+                    }
+
+                    if (newAwards.length > 0) {
+                        var confirmPopup = $ionicPopup.confirm({
+                            title: 'New awards!!!',
+                            template: 'You have recieved a new award!<br/><i class="icon ion-trophy"></i><br/>Would you like to visit the awards page?',
+                        });
+
+                        confirmPopup.then(function (res) {
+                            if (res) {
+                                $state.go('app.awards');
+                            } else {
+
+                            }
+                        });
+                    }
+
+                    resolve(true);
+                }, function (error) {
+                    reject(false);
+                })
             }, function (error) {
                 reject(false);
             })
-        }, function (error) {
-            reject(false);
         })
+    };
+
+    service.canBeAwarded = function (award) {
+        var d = new Date();
+        var nextAwardDate = new Date(award.lastAwarded.getTime() + award.timeDelay);
+
+        if (d > nextAwardDate) {
+            return true;
+        }
+        return false;
     };
 
     //create award
     service.createAward = function (awardName, awardDescription, timeDelay) {
-        var d = new Date();
+        var d = new Date(0);
         var t = d.getTime();
         return $q(function (resolve, reject) {
             service.db.executeSql("INSERT INTO awards_table (awardName, awardDescription, count, lastAwarded, timeDelay) " +
@@ -108,6 +173,7 @@
             service.db.executeSql("INSERT INTO productivity_transactions (pointsAwarded, completionDate) " +
                 "VALUES (?, ?);", [points, dateMillis], function (res) {
                     console.log("Points added to DB: " + points);
+                    service.checkForAwardCompletion();
                     resolve(true);
                 }, function (error) {
                     console.log('INSERT error: ' + error.message);
