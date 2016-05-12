66bc977f1e5e2c80589a520ba88b82fc150d2f68
Alex Scott <0AlexScott@gmail.com>
2016-05-12 11:51:25 +0100

    Added chart in productivity tracker and implemented complimentary message generator

--- www/js/controllers/trackerController.js
+++ www/js/controllers/trackerController.js
@@ -3,9 +3,9 @@
      function ($scope, listStorageService, productivityService) {
 
          $scope.model = {};
-         $scope.labels = ['6', '5', '4', '3', '2', '1', '0'];
-
+         $scope.labels = ['0', '1', '2', '3', '4', '5'];
          $scope.data = [];
+         $scope.message = "";
 
          //add a 'loader' to page as it is opening
          $scope.$on('$ionicView.beforeEnter', function (e) {
@@ -13,9 +13,21 @@
 
          //init tasks and add them to their respective lists when the view is entered
          $scope.$on('$ionicView.enter', function (e) {
-             productivityService.getPointsOverXWeeks(7).then(function (pointsByWeek) {
+             productivityService.getPointsOverXWeeks(6).then(function (pointsByWeek) {
                  $scope.data = [pointsByWeek];
+                 $scope.createMessage();
              }, function (error) { });
          });
+
+
+         //generate complimentary message
+         $scope.createMessage = function () {
+             if ($scope.data[0][0] > $scope.data[0][5]) {
+                 var x = $scope.data[0][0] - $scope.data[0][5];
+                 $scope.message = "Congratulations!<br/>Your productivity has boosted by " + x + "<img src=\"img/pp.png\" style=\"width:16px;height:16px;\" /> since 5 weeks ago!";
+             } else {
+                 $scope.message = "Well done, keep up the good work!";
+             }
+         };
 
      }]);
